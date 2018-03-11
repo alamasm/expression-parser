27d3a08be50ca8d4592a44d4f0d86adc0eb03f18
alamasm <37088318+alamasm@users.noreply.github.com>
2018-03-11 13:38:54 +0300

    Add files via upload
    Changed name from PostfixParser, that used because parser works through postfix notation, to InfixParser, because it parsing infix expressions

--- src/Test/Main.kt
+++ src/Test/Main.kt
@@ -1,11 +1,10 @@
 package Test
 
-import Parser.PostfixParser
-import Math.Utils.*
+import Parser.InfixParser
 import Math.StandartOperationPerformer
 
 fun main(s: Array<String>) {
-    val parser = PostfixParser(StandartOperationPerformer())
+    val parser = InfixParser(StandartOperationPerformer())
     //println("RESULT " + parser.calculate("√ cos ( 0 )"))
     //println("RES " + parser.calculate("3 - ( 5 + 2 )"))
     //println("RES " + parser.calculate("√ ( abs ( 5 + ( 2 ^ 4 * √ 3 * cos ( 5 * √ 9 ) ) / 2 ) )"))
